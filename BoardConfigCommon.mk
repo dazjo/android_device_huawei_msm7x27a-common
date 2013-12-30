bcb8e4c5debbcda60b20353f9425ae394e8391e8
Daz Jones <yuki@thebronasium.com>
2013-12-30 18:03:41 +0100

    BoardConfigCommon: specify legacy audio variant and legacy alsa

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -56,6 +56,10 @@ BOARD_WANTS_EMMC_BOOT := true
 TARGET_GLOBAL_CFLAGS += -mfloat-abi=softfp -mfpu=neon -mtune=cortex-a5
 TARGET_GLOBAL_CPPFLAGS += -mfloat-abi=softfp -mfpu=neon -mtune=cortex-a5
 
+# Audio
+BOARD_USES_LEGACY_ALSA_AUDIO := true
+TARGET_QCOM_AUDIO_VARIANT := legacy
+
 # Bluetooth
 BOARD_HAVE_BLUETOOTH := true
 
