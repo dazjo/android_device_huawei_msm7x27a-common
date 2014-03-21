ee8c176c12d40faa5da9a63529f07cef19596929
Daz Jones <yuki@thebronasium.com>
2014-03-21 13:58:36 +0100

    BoardConfigCommon: enable qc av enhancements

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -86,6 +86,7 @@ TARGET_KERNEL_SOURCE := kernel/huawei/huawei-kernel-3.4
 TARGET_PROVIDES_LIBLIGHTS := true
 
 # Media
+TARGET_ENABLE_QC_AV_ENHANCEMENTS := true
 TARGET_QCOM_MEDIA_VARIANT := legacy
 
 # Partitions
